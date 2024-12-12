4f5881f01eddc9725c53637c00580865d96c4862
yannick nahimana <ynahiman@gmail.com>
2024-12-11 21:45:46 -0500

    fix dropdown menu

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="absolute top-0 left-0 w-full">
-      <div className="p-2 tablet:p-4 flex justify-between items-center">
+      <div className="p-2 tablet:p-4 flex justify-between items-start laptop:items-center">
         <Link href={"/"}>
           <Image
             src={logo}
@@ -53,25 +53,71 @@ const DesktopMenu = () => {
   );
 };
 
+// const MobileMenu = () => {
+//   return (
+//     <div className="drawer drawer-end">
+//       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+//       <div className="drawer-content">
+//         <label
+//           htmlFor="my-drawer-4"
+//           className="drawer-button btn bg-black bg-opacity-60 p-2 "
+//         >
+// <svg
+//   className=" fill-accentCol"
+//   xmlns="http://www.w3.org/2000/svg"
+//   width="32"
+//   height="32"
+//   viewBox="0 0 512 512"
+// >
+//   <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+// </svg>
+//         </label>
+//       </div>
+//       <div className="drawer-side z-10">
+//         <label
+//           htmlFor="my-drawer-4"
+//           aria-label="close sidebar"
+//           className="drawer-overlay"
+//         ></label>
+//         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+//           {/* Sidebar content here */}
+// <li>
+//   <Link href={"/"}>Home</Link>
+// </li>
+// <li>
+//   <Link href={"/shop"}>Shop</Link>
+// </li>
+// <li>
+//   <Link href={"/about"}>Our Story</Link>
+// </li>
+// <li>
+//   <Link href={"/contact"}>Contact Us</Link>
+// </li>
+// <li>
+//   <Link href={"/contact"}>Careers</Link>
+// </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
 const MobileMenu = () => {
-  const [checked, setChecked] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggle = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <div className="drawer drawer-end">
-      <input
-        id="my-drawer-4"
-        type="checkbox"
-        className="drawer-toggle"
-        defaultChecked={checked}
-        onChange={() => setChecked(!checked)}
-      />
-      <div className="drawer-content " onClick={() => setChecked(!checked)}>
-        <label
-          htmlFor="my-drawer-4"
-          className="drawer-button btn bg-black bg-opacity-60 p-2"
-        >
+    <div className="flex flex-col items-end">
+      <div
+        className="btn m-1 bg-black bg-opacity-60 active:bg-black active:bg-opacity-60 hover:bg-opacity-60 hover:bg-black p-2 shadow"
+        onClick={toggle}
+      >
+        {!menuOpen && (
           <svg
-            className="swap-off fill-accentCol"
+            className=" fill-accentCol"
             xmlns="http://www.w3.org/2000/svg"
             width="32"
             height="32"
@@ -79,44 +125,38 @@ const MobileMenu = () => {
           >
             <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
           </svg>
-        </label>
+        )}
+        {menuOpen && (
+          <svg
+            className="fill-accentCol"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        )}
       </div>
-      <div className="drawer-side z-10">
-        <label
-          htmlFor="my-drawer-4"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-          onClick={() => setChecked(!checked)}
-        ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          {/* Sidebar content here */}
-          <li>
-            <Link href={"/"} onClick={() => setChecked(!checked)}>
-              Home
-            </Link>
+      {menuOpen && (
+        <ul className=" menu bg-base-100 rounded-box z-[10] w-[150px] p-2 shadow">
+          <li onClick={toggle}>
+            <Link href={"/"}>Home</Link>
           </li>
-          <li>
-            <Link href={"/shop"} onClick={() => setChecked(!checked)}>
-              Shop
-            </Link>
+          <li onClick={toggle}>
+            <Link href={"/shop"}>Shop</Link>
           </li>
-          <li>
-            <Link href={"/about"} onClick={() => setChecked(!checked)}>
-              Our Story
-            </Link>
+          <li onClick={toggle}>
+            <Link href={"/about"}>Our Story</Link>
           </li>
-          <li>
-            <Link href={"/contact"} onClick={() => setChecked(!checked)}>
-              Contact Us
-            </Link>
+          <li onClick={toggle}>
+            <Link href={"/contact"}>Contact Us</Link>
           </li>
-          <li>
-            <Link href={"/contact"} onClick={() => setChecked(!checked)}>
-              Careers
-            </Link>
+          <li onClick={toggle}>
+            <Link href={"/contact"}>Careers</Link>
           </li>
         </ul>
-      </div>
+      )}
     </div>
   );
 };
